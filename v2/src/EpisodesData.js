a4d4c1b06f5419f0f47acb9512907b32eb503e04
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-08-24 23:02:28 +0300

    v2 - v1.2.0

--- v2/src/EpisodesData.js
+++ v2/src/EpisodesData.js
@@ -1,102 +1,129 @@
 const EpisodesData = {
-  ep50: {
-    cover: "./assets/Darknet-Diaries/ep50.jpg",
-    title: "OPERATION GLOWING SYMPHONY",
-    date: "2019-10-29",
-    epNum: 50,
+  ep60: {
+    cover: "./assets/Darknet-Diaries/ep60.jpg",
+    title: "dawgyg",
+    date: "03 March 2020 ",
+    duration: "43:08",
+    epNum: 60,
     audioSource:
-      "https://dcs.megaphone.fm/ADV5529569827.mp3?key=ff0369ebaed330969074754fca2e6022&request_event_id=de02223a-7ab5-4973-8c4c-7603519e4ca2",
+      "https://dcs.megaphone.fm/ADV9957189315.mp3?key=f77956344842f3f4dc341df1910d5c8d&request_event_id=0ad25894-7956-4f7d-a8b0-74bf3c83fc48",
     description:
-      "Operation Inherent Resolve was started in 2016 which aimed to combat ISIS. It was a combined joint task force lead by the US military. Operation Inherent Resolve sent troops, ships, and air strikes to Iraq and Syria to fire weapons upon ISIS military. It’s widely known that US military engaged with ISIS in this way. But what you may not have heard, is the story of how the US military also combated ISIS over the Internet. This is the story of how the US hacked ISIS and conducted Operation Glowing Symphony.",
+      "This is a story about the hacker named “dawgyg” and how he made over $100,000 in a single day, from hacking.",
   },
-  ep51: {
-    cover: "./assets/Darknet-Diaries/ep51.jpg",
-    title: "SHADOW BROKERS",
-    date: "2019-12-10",
-    epNum: 51,
+
+  ep59: {
+    cover: "./assets/Darknet-Diaries/ep59.jpg",
+    title: "The Courthouse",
+    date: "18 February 2020 ",
+    duration: "85:51",
+    epNum: 59,
     audioSource:
-      "https://dcs.megaphone.fm/ADV9906244248.mp3?key=ffd686a3fbac097d557630fc768d9ecb&request_event_id=2ec37ca4-8981-4d15-a12b-93c6c3edb956",
-    description: "",
+      "https://dcs.megaphone.fm/ADV2874730743.mp3?key=9f2a062e961c5153273f75a4ca65d1c9&request_event_id=0da231d9-d3e8-497c-a9dc-a6bcf3d0c29d",
+    description:
+      "In this episode we hear from Gary and Justin. Two seasoned penetration testers who tell us a story about the time when they tried to break into a courthouse but it went all wrong.",
   },
-  ep52: {
-    cover: "./assets/Darknet-Diaries/ep52.jpg",
-    title: "SHADOW BROKERS",
-    date: "2019-12-10",
-    epNum: 52,
+
+  ep58: {
+    cover: "./assets/Darknet-Diaries/ep58.jpg",
+    title: "OxyMonster",
+    date: "04 February 2020 ",
+    duration: "60:42",
+    epNum: 58,
     audioSource:
-      "https://dcs.megaphone.fm/ADV9906244248.mp3?key=ffd686a3fbac097d557630fc768d9ecb&request_event_id=2ec37ca4-8981-4d15-a12b-93c6c3edb956",
-    description: "",
+      "https://dcs.megaphone.fm/ADV3598870537.mp3?key=1dd7f75e2219c423ddd35ba6d0e5721c&request_event_id=c4c35924-71f7-4ddc-995a-b5dd4249a5b0",
+    description:
+      "OxyMonster sold drugs on the dark web marketplace Dream Market. Something happened though, and it all came crashing down.",
   },
-  ep53: {
-    cover: "./assets/Darknet-Diaries/ep53.jpg",
-    title: "SHADOW BROKERS",
-    date: "2019-12-10",
-    epNum: 53,
+
+  ep57: {
+    cover: "./assets/Darknet-Diaries/ep57.jpg",
+    title: "MS08-067",
+    date: "21 January 2020 ",
+    duration: "58:32",
+    epNum: 57,
     audioSource:
-      "https://dcs.megaphone.fm/ADV9906244248.mp3?key=ffd686a3fbac097d557630fc768d9ecb&request_event_id=2ec37ca4-8981-4d15-a12b-93c6c3edb956",
-    description: "",
-  },
-  ep54: {
-    cover: "./assets/Darknet-Diaries/ep54.jpg",
-    title: "NOTPETYA",
-    date: "2019-12-24",
-    epNum: 54,
-    audioSource:
-      "https://dcs.megaphone.fm/ADV7741006833.mp3?key=93e5b97ee08a967f068459577d75a582&request_event_id=ae0aeca7-405c-4c93-8b7a-aa1e06b7c878",
-    description: "",
-  },
-  ep55: {
-    cover: "./assets/Darknet-Diaries/ep55.jpg",
-    title: "NOTPETYA",
-    date: "2019-12-24",
-    epNum: 55,
-    audioSource:
-      "https://dcs.megaphone.fm/ADV7741006833.mp3?key=93e5b97ee08a967f068459577d75a582&request_event_id=ae0aeca7-405c-4c93-8b7a-aa1e06b7c878",
-    description: "",
+      "https://dcs.megaphone.fm/ADV3206792063.mp3?key=6c16d685d494a040da8d66c3d58a1cfb&request_event_id=6b4414b7-ea41-43bb-a51c-f3e3cacf5cf2",
+    description:
+      "Hear what goes on internally when Microsoft discovers a major vulnerability within Windows.",
   },
   ep56: {
     cover: "./assets/Darknet-Diaries/ep56.jpg",
-    title: "NOTPETYA",
-    date: "2019-12-24",
+    title: "Jordan",
+    date: "07 January 2020 ",
+    duration: "44:10",
     epNum: 56,
     audioSource:
-      "https://dcs.megaphone.fm/ADV7741006833.mp3?key=93e5b97ee08a967f068459577d75a582&request_event_id=ae0aeca7-405c-4c93-8b7a-aa1e06b7c878",
-    description: "",
+      "https://dcs.megaphone.fm/ADV4333614155.mp3?key=46b8d7c4ceb3d3bf3af1421c588ca031&request_event_id=9d3ca423-a15d-4fb0-941f-5cde0fc92800",
+    description:
+      "This is the story of Jordan Harbinger. A bit of a misfit teenager, who was always on the edge of trouble. In this story we hear what happened that lead to a visit from the FBI.",
   },
-  ep57: {
-    cover: "./assets/Darknet-Diaries/ep57.jpg",
-    title: "NOTPETYA",
-    date: "2019-12-24",
-    epNum: 57,
+
+  ep55: {
+    cover: "./assets/Darknet-Diaries/ep55.jpg",
+    title: "NoirNet",
+    date: "25 December 2019 ",
+    duration: "17:37",
+    epNum: 55,
     audioSource:
-      "https://dcs.megaphone.fm/ADV7741006833.mp3?key=93e5b97ee08a967f068459577d75a582&request_event_id=ae0aeca7-405c-4c93-8b7a-aa1e06b7c878",
-    description: "",
+      "https://dcs.megaphone.fm/ADV9304669806.mp3?key=07e9d9857e9f4fbdc75aff3ff2648c7a&request_event_id=a27155a5-f0a5-4f7e-b65d-45bfb651e9c1",
+    description:
+      "A holiday special episode. A private pen tester takes on a job that involves him with another eccentric pen tester, a mischievious smile, and his quest to gain access to the network.",
   },
-  ep58: {
-    cover: "./assets/Darknet-Diaries/ep58.jpg",
-    title: "NOTPETYA",
-    date: "2019-12-24",
-    epNum: 58,
+  ep54: {
+    cover: "./assets/Darknet-Diaries/ep54.jpg",
+    title: "NotPetya",
+    date: "24 December 2019 ",
+    duration: "59:29",
+    epNum: 54,
     audioSource:
-      "https://dcs.megaphone.fm/ADV7741006833.mp3?key=93e5b97ee08a967f068459577d75a582&request_event_id=ae0aeca7-405c-4c93-8b7a-aa1e06b7c878",
-    description: "",
+      "https://dcs.megaphone.fm/ADV7741006833.mp3?key=cad673e25727c42135ce8b1539e26993&request_event_id=3e564627-f735-4f1e-8cac-beb1aa33b9d3",
+    description:
+      "This is the story of NotPetya. Which seems to be the first time we’ve seen what a cyber war looks like. In the summer of 2017 Ukraine suffered a serious and catastrophic cyber attack on their whole country. Hear how it went down, what got hit, and who was responsible.",
   },
-  ep59: {
-    cover: "./assets/Darknet-Diaries/ep59.jpg",
-    title: "NOTPETYA",
-    date: "2019-12-24",
-    epNum: 59,
+
+  ep53: {
+    cover: "./assets/Darknet-Diaries/ep53.jpg",
+    title: "Shadow Brokers",
+    date: "10 December 2019 ",
+    duration: "50:04",
+    epNum: 53,
     audioSource:
-      "https://dcs.megaphone.fm/ADV7741006833.mp3?key=93e5b97ee08a967f068459577d75a582&request_event_id=ae0aeca7-405c-4c93-8b7a-aa1e06b7c878",
-    description: "",
+      "https://dcs.megaphone.fm/ADV9906244248.mp3?key=4ad631a1793296395a506a6a5c6c6eeb&request_event_id=79dd5f65-5b6c-485f-a063-2b4e42b1b776",
+    description:
+      "The NSA has some pretty advanced, super secret, hacking tools. What if these secret hacking tools were to end up in the wrong person’s hands? Well, that happened.",
   },
-  ep60: {
-    cover: "./assets/Darknet-Diaries/ep60.jpg",
-    title: "NOTPETYA",
-    date: "2019-12-24",
-    epNum: 60,
+  ep52: {
+    cover: "./assets/Darknet-Diaries/ep52.jpg",
+    title: "Magecart",
+    date: "26 November 2019 ",
+    duration: "48:17",
+    epNum: 52,
     audioSource:
-      "https://dcs.megaphone.fm/ADV7741006833.mp3?key=93e5b97ee08a967f068459577d75a582&request_event_id=ae0aeca7-405c-4c93-8b7a-aa1e06b7c878",
-    description: "",
+      "https://dcs.megaphone.fm/ADV1104544195.mp3?key=c4429c8e461d722f41d40997bd848dbd&request_event_id=8f34cc37-0d47-4baf-b8ad-9ffed18792de",
+    description:
+      "Credit card skimming is growing in popularity. Gas pumps all over are seeing skimmers attached to them. It’s growing in popularity because it’s really effective. Hackers have noticed how effective it is and have began skimming credit cards from websites.",
+  },
+
+  ep51: {
+    cover: "./assets/Darknet-Diaries/ep51.jpg",
+    title: "The Indo-Pak Conflict",
+    date: "12 November 2019 ",
+    duration: "46:49",
+    epNum: 51,
+    audioSource:
+      "https://dcs.megaphone.fm/ADV5003101655.mp3?key=9b87b457621527bfee36952b3f4de7fe&request_event_id=28647b63-5a03-483a-8ab0-bedcd98f912a",
+    description:
+      "Kashmir is a region right in between India, Pakistan, and China. For the last 70 years Pakistan and India have fought over this region of the world, both wanting to take control of it. Tensions sometimes heat up which can result in people being killed. When tensions get high in the real world, some people take to the internet and hack their rivals as a form of protest. In this episode we’ll explore some of the hacking that goes on between India and Pakistan.",
+  },
+  ep50: {
+    cover: "./assets/Darknet-Diaries/ep50.jpg",
+    title: "Operation Glowing Symphony",
+    date: "29 October 2019 ",
+    duration: "71:10",
+    epNum: 50,
+    audioSource:
+      "https://dcs.megaphone.fm/ADV5529569827.mp3?key=0cb46372663bcf7c4d62b4060292cffe&request_event_id=46d107fd-34c5-437e-8f5f-9718659b7ca5",
+    description:
+      "Operation Inherent Resolve was started in 2016 which aimed to combat ISIS. It was a combined joint task force lead by the US military. Operation Inherent Resolve sent troops, ships, and air strikes to Iraq and Syria to fire weapons upon ISIS military. It’s widely known that US military engaged with ISIS in this way. But what you may not have heard, is the story of how the US military also combated ISIS over the Internet. This is the story of how the US hacked ISIS and conducted Operation Glowing Symphony.",
   },
 };
--- v2/src/renderer.js
+++ v2/src/renderer.js
@@ -1,8 +1,8 @@
 const Main = document.querySelector("#main > div.grid");
 
-function ep(cover, title, date, epNum, audioSource, description) {
+function ep(cover, title, date, duration, epNum, audioSource, description) {
   return `
-    <div class="group relative rounded-sm overflow-hidden">
+    <div class="group relative rounded-sm overflow-hidden hover:drop-shadow-[0_1px_10px_#ff000055] transition delay-75 duration-500 brightness-50 hover:brightness-75">
       <span class="absolute z-10 top-1 left-0 text-[#f00] px-2 rounded-e-lg text-xs bg-black">
         EP ${epNum}
       </span>
@@ -11,6 +11,7 @@ function ep(cover, title, date, epNum, audioSource, description) {
       </span>
       <span class="hidden">${date}</span>
       <span class="hidden">${audioSource}</span>
+      <span class="hidden">${duration}</span>
       <span class="hidden">${description}</span>
       <img src=${cover} class="object-cover w-full h-full" />
     </div>
@@ -18,14 +19,68 @@ function ep(cover, title, date, epNum, audioSource, description) {
 }
 
 for (const key in EpisodesData) {
-  const [cover, title, date, epNum, audioSource, description] = [
+  const [cover, title, date, duration, epNum, audioSource, description] = [
     EpisodesData[key]["cover"],
     EpisodesData[key]["title"],
     EpisodesData[key]["date"],
+    EpisodesData[key]["duration"],
     EpisodesData[key]["epNum"],
     EpisodesData[key]["audioSource"],
     EpisodesData[key]["description"],
   ];
 
-  Main.innerHTML += ep(cover, title, date, epNum, audioSource, description);
+  Main.innerHTML += ep(
+    cover,
+    title,
+    date,
+    duration,
+    epNum,
+    audioSource,
+    description
+  );
 }
+
+/*
+
+function getData() {
+  const [title, date, description] = [
+    document.querySelector("body > div.page  h1").textContent,
+    document.querySelector(
+      "body > div.page > div > section.hero.hero--single > div.wrap > p"
+    ).textContent,
+    document.querySelector(
+      "body > div.page > div > section.single > div > article > p:nth-child(5)"
+    ).textContent,
+  ];
+
+  return {ep: {
+    cover: `./assets/Darknet-Diaries/ep${title.slice(
+      3,
+      title.indexOf(":")
+    )}.jpg`,
+    title: title.slice(title.indexOf(":") + 2),
+    date: date.slice(0, date.indexOf("|")),
+    duration: date.slice(date.indexOf("|") + 2),
+    epNum: +title.slice(3, title.indexOf(":")),
+    audioSource: "",
+    description: description,
+  }};
+}
+getData();
+
+
+*/
+
+/*
+{
+    "ep": {
+        "cover": "./assets/Darknet-Diaries/ep50.jpg",
+        "title": "Operation Glowing Symphony",
+        "date": "29 October 2019 ",
+        "duration": "71:10",
+        "epNum": 50,
+        "audioSource": "",
+        "description": "Operation Inherent Resolve was started in 2016 which aimed to combat ISIS. It was a combined joint task force lead by the US military. Operation Inherent Resolve sent troops, ships, and air strikes to Iraq and Syria to fire weapons upon ISIS military. It’s widely known that US military engaged with ISIS in this way. But what you may not have heard, is the story of how the US military also combated ISIS over the Internet. This is the story of how the US hacked ISIS and conducted Operation Glowing Symphony."
+    }
+}
+*/
