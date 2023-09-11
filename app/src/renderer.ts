9244671b5e05a0ddd6be3175a41692a8388db164
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-09-11 06:27:32 +0300

    v1.5.1
    Add the old code

--- app/src/renderer.ts
+++ app/src/renderer.ts
@@ -1,31 +1,37 @@
-/**
- * This file will automatically be loaded by vite and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.ts` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
+import "./index.css";
 
-import './index.css';
+import playPauseBtnClickHandler from "./components/playPauseBtnClickHandler";
+import episode from "./components/episode";
+import EpisodesData from "./components/EpisodesData";
+import likeBtnClickHandler from "./components/likeBtnClickHandler";
+// ---------------------------- //
 
-console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+const Main = document.querySelector("#main > div.grid");
+
+for (const key in EpisodesData) {
+  const [cover, title, date, duration, epNum, audioSource, description] = [
+    EpisodesData[key]["cover"],
+    EpisodesData[key]["title"],
+    EpisodesData[key]["date"],
+    EpisodesData[key]["duration"],
+    EpisodesData[key]["epNum"],
+    EpisodesData[key]["audioSource"],
+    EpisodesData[key]["description"],
+  ];
+
+  Main.innerHTML += episode(
+    cover,
+    title,
+    date,
+    duration,
+    epNum,
+    audioSource,
+    description
+  );
+}
+
+// ---------------------------- //
+
+likeBtnClickHandler();
+
+playPauseBtnClickHandler();
